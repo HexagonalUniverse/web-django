b13e4b005f0866178eaa080f36c0d2e6012103b4
HexagonalUniverse <hebertluizhd78@hotmail.com>
2026-06-28 21:51:25 -0300

    3
    também, comittando as _migrations_ só em caso...
    No roteiro anterior eu não o fiz por não julgar necessário; mas pode ser que seja bom pra ver o progresso...

--- home/models.py
+++ home/models.py
@@ -7,10 +7,13 @@ class Mensagem(models.Model):
     conteudo: models.Field = models.TextField()
     criada_em: models.Field = models.DateTimeField(auto_now_add=True)
 
+    # NOVO CAMPO
+    autor: models.Field = models.CharField(max_length=80, default="Anônimo")
+
     class Meta:
         ordering = ['-criada_em',]
 
     def __str__(self) -> str:
-        return titulo
+        return self.titulo
 
 
